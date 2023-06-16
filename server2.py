1a035d2b638e45132ea816e3a9e1f646c642cb34
SHEHZAD <99957366+muhammadshehzad038@users.noreply.github.com>
2023-06-16 11:16:55 -0700

    Add files via upload

---
+++ server2.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/members")
+def members():
+    return jsonify({"message": "Hello! My name is Shehzad"})
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000, host='0.0.0.0')
